cd2a577175fa68fd167cc59390c99b182ec16a76
FuryaevStas <furyaev.stanislav@gmail.com>
2017-04-08 21:22:30 +0300

    fixes

--- core.cpp
+++ core.cpp
@@ -1,5 +1,3 @@
-#include	<stdio.h>
-
 #include	"core.h"
 
 int main(int argc, char *argv[]) {
--- core.h
+++ core.h
@@ -1,8 +1,8 @@
 #ifndef _CORE_H_
 #define _CORE_H_
 
+#include	<stdio.h>
+#include	<stdlib.h>
 #include	<ncurses.h>
-
-
 
 #endif
